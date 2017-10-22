dd80bfdb03c44e2613b63638d043504bf053de57
Harimaron <gaetan.ribonnet@outlook.com>
2017-10-22 15:08:54 +0200

    +12+13

--- 12-MOUSE-EVENT/app.js
+++ 12-MOUSE-EVENT/app.js
@@ -1,12 +1,18 @@
 document.querySelectorAll('.hoverMe').forEach((element) => {
     element.addEventListener('mouseover', (event) => {
-        event.target.style.color = 'white';
+        event.target.style.opacity = 0;
     });
 });
 
-document.querySelectorAll('.hoverMe').forEach((element) => {
-    element.addEventListener('click', (event) => {
-        event.target.style.color = 'black';
+document.getElementById("reset").addEventListener('click', (event) => {
+    document.querySelectorAll('.hoverMe').forEach((element) => {
+        element.style.opacity = 1;
+    });
+});
 
-    })
+var axeX = document.getElementById('axe-x');
+var axeY = document.getElementById('axe-y');
+window.addEventListener('mousemove', (event) => {
+    axeX.textContent = event.clientX;
+    axeY.textContent = event.clientY;
 });
